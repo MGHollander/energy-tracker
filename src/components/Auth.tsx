dbcb79148e7b2f2cd77c353d56af755f49c113b6
Marc Hollander <1532664+MGHollander@users.noreply.github.com>
2026-01-15 13:12:39 +0100

    Styling

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -29,7 +29,7 @@ export default function Auth() {
 
     if (user) {
         return (
-            <div className="flex items-center justify-between bg-white dark:bg-gray-800 rounded-lg shadow-md p-4">
+            <div className="flex items-center justify-between bg-white dark:bg-gray-800 rounded-lg shadow-md p-4 gap-4 text-xs">
                 <div>
                     <p className="text-gray-900 dark:text-white">
                         Logged in as: <span className="font-medium">{user.email}</span>
